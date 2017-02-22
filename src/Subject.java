3cf6669256745d9ed784c0bb1225512008ecff13
Flamur Qorrolli <qorrolliflamur@gmail.com>
2017-02-22 23:13:44 +0100

    Subject class

---
+++ src/Subject.java
@@ -0,0 +1,35 @@
+import entities.AbstractEntity;
+public class Subject extends AbstractEntity {
+
+    String name;
+    String institutionId;
+    String SyllabusId;
+
+
+    public String getSyllabusId() {
+        return SyllabusId;
+    }
+
+    public void setSyllabusId(String syllabus) {
+        SyllabusId = syllabus;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getInstitutionId() {
+        return institutionId;
+    }
+
+    public void setInstitutionId(String institutionId) {
+        this.institutionId = institutionId;
+    }
+
+
+
+}
